85d5b974c6285cb63c57cb91bd4577d2aa94dd73
Justine Dave JOAQUIN <justin.joaquin@ext.essilor.com>
2023-04-25 14:28:15 +0800

    modified header text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const Header: FC = () => {
   return (
     <nav className="navbar">
-      <h1>CRUD App</h1>
+      <h1>CRUD Application</h1>
       <div className="navbar__user-info">
         <p className="user-info__user-name">John Doe</p>
         <p className="user-info__user-type">Admin</p>
