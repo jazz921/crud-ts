8a9d33d307ac6d02b0a21d5cd71ca73e9301ee0f
Justine Dave JOAQUIN <justin.joaquin@ext.essilor.com>
2023-05-23 13:12:07 +0800

    add name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: FC = () => {
     <nav className="navbar">
       <h1>CRUD Application w/ GTM</h1>
       <div className="navbar__user-info">
-        <p className="user-info__user-name">John Doe</p>
+        <p className="user-info__user-name">Markus Adamson</p>
         <p className="user-info__user-type">Admin</p>
       </div>
     </nav>
